ac916d3daf0bf9ea03e402c4d88133bc98f6bfb8
yjsoon <yjsoon@tinkertanker.com>
2010-07-12 00:49:16 +0800

    RSS of linked list now adds a permalink to blog post, and non-linked list posts now have a glyph in front (i.e. closer to Daring Fireball style).

--- linked_list.php
+++ linked_list.php
@@ -1,27 +1,25 @@
 <?php
 /**
- * @package Linked List
- * @author Jonathan Penn
- * @version 1.0
+ * @package DF Linked List
+ * @author YJ Soon
+ * @version 1.1
  */
 /*
 Plugin Name: Linked List
-Plugin URI: http://github.com/jonathanpenn/wordpress-linked-list-plugin
-Description: Have designated posts act like the "Linked List" posts on Daring Fireball. Set the custom field "linked_list_url" to any desired destination on a post. The permalink then becomes that destination. Affects RSS feeds automatically and provides functions you can use in your template to control the layout if the post is a normal post, or a linked-list post".
-Author: Jonathan Penn
-Version: 1.0
-Author URI: http://wavethenavel.com
+Plugin URI: http://github.com/yjsoon/wordpress-linked-list-plugin
+Description: Adapted from Jonathan Penn's <a href="http://github.com/jonathanpenn/wordpress-linked-list-plugin">linked-list plugin</a> to make your blog's RSS feed behave even more like <a href="http://daringfireball.net">Daring Fireball</a>. To use, set the custom field "linked_list_url" to the desired location on a link post. In your RSS feed, the following will happen: (1) the permalink becomes the link destination; (ii) the actual permalink to your post is inserted as a star glyph at the end of your post; (iii) a star glyph is added to before your non-linked-list post titles. Also provides functions to customise your design by checking if the item is a linked list item, getting a permalink with glyph, etc. 
+Author: Yinjie Soon
+Version: 1.1
+Author URI: http://yjsoon.com
 */
 
 // Called to see if the current post in the loop is a linked list
 function is_linked_list()
 {
-  $url = get_post_custom_values('linked_list_url');
-  if (empty($url)) {
-    return false;
-  } else {
-    return true;
-  }
+  global $wp_query;
+  $postid = $wp_query->post->ID;
+  $url = get_post_meta($postid, 'linked_list_url', true);
+  return (!empty($url));
 }
 
 function get_the_linked_list_link()
@@ -46,5 +44,49 @@ function ensure_rss_linked_list($value) {
 
 // Now we set that function up to execute when the admin_footer action is called
 add_action('the_permalink_rss', 'ensure_rss_linked_list');
+
+
+/*-----------------------------------------------------------------------------
+  Enhanced linked list functions
+-----------------------------------------------------------------------------*/
+
+// To display a permalink glyph
+function the_permalink_glyph()
+{
+  echo get_the_permalink_glyph();
+}
+
+function get_the_permalink_glyph()
+{
+  return '<a href="' . get_permalink() . '" rel="bookmark" title="Permanent link to \''.get_the_title().'\'" class="glyph">&nbsp;'. get_glyph() .'&nbsp;</a>';
+}
+
+function get_glyph() {
+  return '&#9733;';
+}
+
+// Inject permalink glyph into RSS feed contents
+function insert_permalink_glyph_rss($content)
+{
+  if (is_linked_list() && is_feed()) 
+  {
+    $content = $content . "<p>" . get_the_permalink_glyph() . "</p>\n";
+  }
+  return $content;
+}
+add_filter('the_content', 'insert_permalink_glyph_rss');
+add_filter('the_excerpt_rss', 'insert_permalink_glyph_rss');
+
+// Inject permalink glyph into RSS title
+function insert_title_glyph_rss($title)
+{
+  if (!is_linked_list()) 
+  {
+    $title = get_glyph() . " " . $title;
+  }
+  return $title;
+}
+add_filter('the_title_rss', 'insert_title_glyph_rss');
+
 
 ?>
